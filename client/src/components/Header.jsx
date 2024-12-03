603ac213aa6343f8668b62a8ed39d5932033175f
Manas Maheshwari <manasmaheshwari2004@gmail.com>
2024-12-03 16:46:02 +0530

    Create and connect database

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
             </Link>
             <form className='bg-slate-50 p-3 rounded-lg flex items-center'>
                 <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-                <img src=".\public\search.png" alt="icon" className='h-6'/>
+                <img src="./search.png" alt="icon" className='h-6'/>
             </form>
             <ul className='flex gap-8'>
                 <Link to='/'>
