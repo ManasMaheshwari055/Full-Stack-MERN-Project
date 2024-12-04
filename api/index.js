6c9b2b5a6f6bd40eeeae4247d2a982d8d4f359ba
Manas Maheshwari <manasmaheshwari2004@gmail.com>
2024-12-04 17:25:15 +0530

    Create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -20,4 +21,7 @@ app.listen(3001, () => {
     }
 );
 
+app.use(express.json());
+
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
